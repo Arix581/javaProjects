d0005ceb77865d92c378e590e44cfd9a4cddbe25
Arix581 <113464283+Arix581@users.noreply.github.com>
2024-10-07 21:43:27 -0600

    hangman.java V0.0.2 Finish majority of game

--- hangman.java
+++ hangman.java
@@ -1,48 +1,67 @@
 import java.util.Scanner;
 
+// V0.0.2 
 public class hangman {
     public static void main(String[] args) {
         /* 
          * Things we have to do:
-         * Get a guess from the player
-         * Add the guess to a list of guesses
-         * if the guess isnt in the word and not in the list of guesses
-         *   remove a life.
-         * For each character of the word
-         *   See if the letter has been guessed
-         *   if it has, it will be displayed
-         *   else it will be replaced by an underscore
-         * print out the guess result
+         * Fix bug with the input system
          */
         System.out.println("Hello World");
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Hangman!");
-        int lives = 3;
-        String secretWord = "test word";
-        String visibleWord = "";
-        String guesses = "";
         
+        // set the visible word to all underscores
+        String secretWord = "This is a test string.".toLowerCase();
+        String visibleWord = secretWord.replaceAll("(?i)[qwertyuiopasdfghjklzxcvbnm]", "_"); 
+        
+        int lives = 3;
+        String guesses = "";
         boolean gameDone = false;
         boolean victory = false;
+        
         while (!gameDone) {
+            // Display the board
+            System.out.println(visibleWord);
+            
             // Make a guess
             System.out.println("Make a guess! (You have " + lives + " lives)");
+            
+            // BUG! If you submit just an enter, it breaks.
+            // Just check if the player submitted a string of length
+            // 0 and then make them reguess before using the guess.
             String guess = scanner.nextLine().substring(0,1);
+            
+            // Repeat prevention
             if (guesses.contains(guess))
             {
                 System.out.println("You have already guessed that.");
                 continue;
+            } else {
+                guesses += guess;
             }
 
             // Check if the guess is in the world.
             if (secretWord.contains(guess)) { // Is in the secret word
                 // Add to the visible word
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if (secretWord.charAt(i) == guess.charAt(0)) {
+                        visibleWord = visibleWord.substring(0,i) + guess + visibleWord.substring(i + 1 , visibleWord.length());
+                    }
+                }
             } else { // Is not in the secret word
-                // Remove life and check for failure.
+                lives--;
+                System.out.println(guess + " is not in the secret word.");
+                if (lives < 1) { // Die
+                    victory = false;
+                    break;
+                } else {
+                    System.out.println("You have " + lives + " lives remaining.");
+                }
             }
-
-            // Display the guessed words thus far.
-        }
+        } // Game Loop Ends
+        
+        // Have different win or loss texts.
     }
 }
