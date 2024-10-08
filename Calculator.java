e029d29544830851dce2c722147451bb3e970984
Arix581 <113464283+Arix581@users.noreply.github.com>
2024-10-08 12:34:24 -0600

    Update Calculator.java V2

--- Calculator.java
+++ Calculator.java
@@ -6,25 +6,48 @@ class Calculator {
     public static void main(String[] args) {
         System.out.println("Welcome to the Calculator");
         Scanner scanner = new Scanner(System.in);
-        
-        // Set the operation and numbers.
-        System.out.println("What is your desired operation? (+-*/)");
-        String operation = scanner.nextLine();
-        System.out.println("Number 1? ");
-        int number1 = scanner.nextInt();
-        System.out.println("Number 2? ");
-        int number2 = scanner.nextInt();
-        
+        while (true) {
+            // Set the operation and numbers.
+            System.out.println("What is your desired operation? (+-*/)");
+            String operation = scanner.nextLine();
+            System.out.println("Number 1? ");
+            int number1 = scanner.nextInt();
+            System.out.println("Number 2? ");
+            int number2 = scanner.nextInt();
+            
+            // Results display
+            evaluate(number1, number2, operation);
+            
+            if (!goAgain()) {
+                break;
+            }
+        }
+    }
+    
+    public static float evaluate(int num1, int num2, String operation) {
         float result = 0f;
         switch (operation) {
-            case "+": result = number1 + number2; break;
-            case "-": result = number1 - number2; break;
-            case "*": result = number1 * number2; break;
-            case "/": result = number1 / number2; break;
+            case "+": result = num1 + num2; break;
+            case "-": result = num1 - num2; break;
+            case "*": result = num1 * num2; break;
+            case "/": 
+                result = (num2 != 0) ? num1 / num2 : 0;
+                break;
             default:
                 System.out.println(operation + " is not an operation supported by this calculator.");
                 break;
         }
         System.out.println("The result is " + result);
+        return result;
+    }
+    
+    public static boolean goAgain() {
+        System.out.println("Would you like to go again?");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.nextLine().startsWith("y")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
